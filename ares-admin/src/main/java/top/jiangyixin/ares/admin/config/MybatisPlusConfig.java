95d1619cab6448fe136a3394cb5d33467620e50c
jiangyixin1209 <17602173915@163.com>
2020-12-21 16:02:19 +0800

    cachetemplate crud

---
+++ ares-admin/src/main/java/top/jiangyixin/ares/admin/config/MybatisPlusConfig.java
@@ -0,0 +1,10 @@
+package top.jiangyixin.ares.admin.config;
+
+/**
+ * TODO
+ * @version 1.0
+ * @author jiangyixin
+ * @date 2020/12/21 下午2:32
+ */
+public class MybatisPlusConfig {
+}
---
+++ ares-admin/src/main/java/top/jiangyixin/ares/admin/pojo/vo/CacheTemplateVO.java
@@ -0,0 +1,10 @@
+package top.jiangyixin.ares.admin.pojo.vo;
+
+/**
+ * TODO
+ * @version 1.0
+ * @author jiangyixin
+ * @date 2020/12/21 下午2:47
+ */
+public class CacheTemplateVO {
+}
